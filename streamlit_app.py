958f7ad680eb11071161769a3f4e10fed987741f
Tin Cabalar <78011994+inhinyeratin@users.noreply.github.com>
2026-03-12 16:01:04 +0800

    Update streamlit_app.py
    corrected the spelling of width

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ ingredients_list = st.multiselect(
 )
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json()) #Additional line
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_widt=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 '''
 --- Submit order ---
 if ingredients_list and name_on_order:
